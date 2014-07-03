caf25ba7394be8dfda92e7ace4c13b928e076188
Tatyana Fonina <tanyafn@gmail.com>
2014-07-03 17:23:48 +1000

    Add options for shortlink

--- lib/shortlink.rb
+++ lib/shortlink.rb
@@ -6,33 +6,33 @@ module Shortlink
   extend ActiveSupport::Concern
 
   module ClassMethods
+    DEFAULT_SETTINGS = { length: 6, prefix: nil }
+
     def shortlink column_name, options = {}
-      cattr_accessor :shortlink_field
-      self.shortlink_field = column_name
-
       include Shortlink::LocalInstanceMethods
-      before_validation :set_shortlink
-    end
 
-    def generate_shortlink
-      chars = [('a'..'z'), ('A'..'Z'), (0..9)].flat_map(&:to_a)
-      chars.sample(6).join
-    end
+      settings = options.merge(DEFAULT_SETTINGS).extract!(*DEFAULT_SETTINGS.keys)
+
+      before_validation { set_shortlink(column_name, settings) }
+    end 
   end
 
   module LocalInstanceMethods
-    def self.included(base)
-      puts base.inspect
-    end
-    def set_shortlink
-      sequence = self.class.generate_shortlink
+    def set_shortlink shortlink_field, options
+      sequence = generate_shortlink(options[:length], options[:prefix])
 
-      while self.class.exists?(self.class.shortlink_field => sequence) 
-        sequence = self.class.generate_shortlink
+      while self.class.exists?(shortlink_field => sequence) 
+        sequence = generate_shortlink(options[:length], options[:prefix])
       end
       
-      self.send("#{self.class.shortlink_field}=", sequence)
+      self.send("#{shortlink_field}=", sequence)
     end
+
+    def generate_shortlink length, prefix
+      chars = [('a'..'z'), ('A'..'Z'), (0..9)].flat_map(&:to_a)
+      prefix ? "#{prefix}-#{chars.sample(length).join}": chars.sample(length).join
+    end   
   end
 end
+
 ActiveRecord::Base.send(:include, Shortlink)
